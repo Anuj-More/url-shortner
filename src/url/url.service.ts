25300993239e005493a40f6d5466ded441a4d79a
Anuj More <work.anujmore@gmail.com>
2026-05-13 22:00:05 +0530

    Revert "implement UrlService with shorten method for URL shortening functionality"
    This reverts commit 35e0376294e2ef844faf38ee96544b7731c5d3b2.

--- src/url/url.service.ts
+++ src/url/url.service.ts
@@ -1,30 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { ShortenUrlDto } from './dto/shorten-url.dto';
-import { Base62 } from 'src/utils/base62.util';
 
 @Injectable()
-export class UrlService {
-    constructor(private prisma: PrismaService) {}
-
-    async shorten(dto: ShortenUrlDto) {
-        const existing = await this.prisma.url.findFirst({
-            where: { longUrl: dto.longUrl },
-        });
-        if (existing) return;
-
-        const urlRecord = await this.prisma.url.create({
-            data: {
-                longUrl: dto.longUrl,
-                shortCode: '',
-            },
-        });
-
-        const shortCode = Base62.encode(urlRecord.id);
-
-        return this.prisma.url.update({
-            where: { id: urlRecord.id },
-            data: { shortCode },
-        });
-    }
-}
+export class UrlService {}

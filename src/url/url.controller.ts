cb3124c74d800e633e7a4101d3ec4a8ae4a5bd01
Anuj More <work.anujmore@gmail.com>
2026-05-13 21:59:36 +0530

    Revert "implement UrlController with shorten and redirect methods for URL handling"
    This reverts commit 6d33755189d297556ddd142b31a20e6303fc1d54.

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -1,21 +1,4 @@
-import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
-import type { Response } from 'express';
-import { ShortenUrlDto } from './dto/shorten-url.dto';
-import { UrlService } from './url.service';
+import { Controller } from '@nestjs/common';
 
 @Controller('url')
-export class UrlController {
-    constructor(private urlService: UrlService) {}
-
-    @Post('shorten')
-    async shorten(@Body() dto: ShortenUrlDto){
-        return this.urlService.shorten(dto);
-    }
-
-    @Get(':code')
-    async redirect(@Param('code') code: string, @Res() res: Response){
-        const originalUrl = await this.urlService.getOriginalUrl(code);
-
-        return res.redirect(302, originalUrl);
-    }
-}
+export class UrlController {}

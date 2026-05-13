ed645b08d4d41282c3b4b3763665eead048e9899
Anuj More <work.anujmore@gmail.com>
2026-05-13 22:00:26 +0530

    Revert "add ShortenUrlDto with validation for long URL"
    This reverts commit af04548d70d5112abd55228bf7f4a3435f591b8f.

--- src/url/dto/shorten-url.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, isNotEmpty, IsUrl } from "class-validator";
-
-
-export class ShortenUrlDto {
-    @IsNotEmpty()
-    @IsUrl({}, {message: "Please provide a valid URL (including http/https)"})
-    longUrl!: string;
-}
